7d4baab771f6a251396224e5aa6628740adcd865
James <j606888@gmail.com>
2025-08-20 13:58:09 +0800

    Add loader

--- src/app/video/[uid]/page.tsx
+++ src/app/video/[uid]/page.tsx
@@ -1,14 +1,21 @@
-"use client"
+"use client";
 
 import { useParams } from "next/navigation";
 import VideoDetail from "@/features/VideoDetail";
 import { useGetVideoQuery } from "@/store/slices/videos";
+import { PulseLoader } from "react-spinners";
 
 const VideoPage = () => {
   const { uid } = useParams();
 
-  const { data: video } = useGetVideoQuery(uid as string);
+  const { data: video, isLoading } = useGetVideoQuery(uid as string);
 
+  if (isLoading)
+    return (
+      <div className="flex justify-center items-center h-screen">
+        <PulseLoader size={20} color="#4A81D9" />
+      </div>
+    );
   if (!video) return <div>No video found</div>;
 
   return <VideoDetail videoUid={video.uid} />;
