8b6357f18f2bdf735099c2f0c8d071c1943e3ae8
James <j606888@gmail.com>
2025-08-20 13:59:50 +0800

    Add more loader

--- src/features/VideoList/index.js
+++ src/features/VideoList/index.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { useGetVideosQuery } from "@/store/slices/videos";
 import VideoPreview from "./VideoPreview";
 import VideoFilter from "./VideoFilter";
+import { PulseLoader } from "react-spinners";
 
 const VideoList = () => {
   const [filters, setFilters] = useState({
@@ -12,15 +13,30 @@ const VideoList = () => {
   const [viewType, setViewType] = useState("grid");
 
   if (isLoading) {
-    return <div>Loading...</div>;
+    return (
+      <div className="flex justify-center items-center h-screen">
+        <PulseLoader size={20} color="#4A81D9" />
+      </div>
+    );
   }
 
   return (
-    <div className={`relative ${viewType === "grid" ? "grid grid-cols-3 gap-[3px]" : "flex flex-col gap-2 py-1"}`}>
+    <div
+      className={`relative ${
+        viewType === "grid"
+          ? "grid grid-cols-3 gap-[3px]"
+          : "flex flex-col gap-2 py-1"
+      }`}
+    >
       {videos?.result?.map((video) => (
-        <VideoPreview key={video.uid} video={video} viewType={viewType}/>
+        <VideoPreview key={video.uid} video={video} viewType={viewType} />
       ))}
-      <VideoFilter filters={filters} setFilters={setFilters} viewType={viewType} setViewType={setViewType} />
+      <VideoFilter
+        filters={filters}
+        setFilters={setFilters}
+        viewType={viewType}
+        setViewType={setViewType}
+      />
     </div>
   );
 };
